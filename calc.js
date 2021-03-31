7b8d682a32f507551a42764d7a3cfda3a0f20329
Adetunji Adebowale <66047485+KoolTeck@users.noreply.github.com>
2021-03-31 16:00:15 +0100

    updating the calc code

---
+++ calc.js
@@ -0,0 +1,54 @@
+// getting the required dom elements
+    const calcForm = document.getElementById('calcForm');
+    let result = document.getElementById('result');
+
+ // setting an event listener on form submit
+ calcForm.addEventListener("submit", calculate);
+
+ function calculate(eve) {
+ 	eve.preventDefault();
+ 	  //getting the needed inputs by user from form
+ 	let firstNum = calcForm.querySelector("[name=num1]").value;
+    let secondNum =  calcForm.querySelector("[name=num2]").value;
+    let operand = calcForm.querySelector("[name=operation]").value;
+ 	if (firstNum == ""  || secondNum == "") {
+ 		alert("first number and second number required");
+ 		return;
+ 	}
+ 	firstNum = parseFloat(firstNum);
+ 	secondNum = parseFloat(secondNum);
+ 	// checking if the input is still a string after conversion
+ 	if (Number.isNaN(firstNum) || Number.isNaN(secondNum)) {
+ 		result.innerHTML = `<h5 style="color: crimson;">please input valid numbers</h5>`;
+ 	} else {
+ 		switch(operand) {
+ 			case "Addition":
+ 			result.innerHTML = `<h5 class="alert-success">
+ 			 ${firstNum} + ${secondNum} =  ${firstNum + secondNum}
+ 			</h5>`;
+ 			calcForm.reset();
+ 			break;
+ 			case "Subtraction":
+ 			result.innerHTML = `<h5 class="alert-success">
+ 			  ${firstNum} - ${secondNum} = 
+ 			   ${firstNum - secondNum}
+ 			</h5>`;
+ 			calcForm.reset();
+ 			break;
+ 			case "Multiply":
+ 			result.innerHTML = `<h5 class="alert-success">
+ 			   ${firstNum} * ${secondNum} = 
+ 			   ${firstNum * secondNum}
+ 			</h5>`;
+ 			calcForm.reset();
+ 			break;
+ 			case "Division":
+ 			result.innerHTML = `<h5 class="alert-success">
+ 			  ${firstNum} / ${secondNum} = 
+ 			   ${firstNum / secondNum}
+ 			</h5>`;
+ 			calcForm.reset();
+ 			break;
+ 		}
+ 	}
+ } 
